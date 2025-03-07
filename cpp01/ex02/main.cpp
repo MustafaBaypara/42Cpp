9ed3e1f04ec650c707f82edab48e54686c3f812b
Mustafa Baypara <musti.baypara@gmail.com>
2025-03-07 22:48:49 +0300

    ex02

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -8,7 +8,7 @@ int main()
 	std::cout << "Memory adress of the string: " << &str << std::endl;
 	std::cout << "Memory adress held by pointer: " << stringPTR << std::endl;
 	std::cout << "Memory adress held by reference; " << &stringREF << std::endl;
-	std::cout << "===========================; " << std::endl;
+	std::cout << "================================" << std::endl;
 	std::cout << "String " << str << std::endl;
 	std::cout << "String of pointed to by pointer: " << *stringPTR << std::endl;
 	std::cout << "String of pointed to by reference: " << stringREF << std::endl;
